c2efefdc539857f0cf48aeead78ebd9a923e8a6e
Szymon Gos <szymonngos@gmail.com>
2020-09-29 13:35:57 +0100

    Split the code into separate functions - JS

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-
 document.addEventListener('DOMContentLoaded', () => {
-    
 
     const rootElement = document.querySelector('#root');
     const sections = document.querySelectorAll('section');
     let currentSectionIndex = 0;
     let isThrotlled = false;
-   
+
+
     document.addEventListener('mousewheel', (e) => {
 
-        if(isThrotlled) return;
+        if (isThrotlled) return;
         isThrotlled = true;
 
         //asynch function
@@ -17,25 +16,35 @@ document.addEventListener('DOMContentLoaded', () => {
             isThrotlled = false;
         }, 1000)
 
-       const direction = e.wheelDelta < 0 ? 1 : -1;
-       if (direction === 1) {
-           const isLastSection = currentSectionIndex === sections.length -1;
-           if(isLastSection) return;
-       } else if (direction === -1){
-           const firstSection = currentSectionIndex === 0;
-           if(firstSection) return;
-       }
+        const direction = e.wheelDelta < 0 ? 1 : -1;
 
-       currentSectionIndex += direction;
-
-       console.log(currentSectionIndex);
-
-
-       sections[currentSectionIndex].scrollIntoView({
-           behavior: "smooth",
-           block: "start",
-       })
+        scroll(direction);
 
     })
+
+    const scroll = direction => {
+
+        if (direction === 1) {
+            const isLastSection = currentSectionIndex === sections.length - 1;
+            if (isLastSection) return;
+        } else if (direction === -1) {
+            const firstSection = currentSectionIndex === 0;
+            if (firstSection) return;
+        }
+
+        currentSectionIndex += direction;
+
+        console.log(currentSectionIndex);
+
+        scrollCurrentSection();
+    }
+
+
+    function scrollCurrentSection() {
+        sections[currentSectionIndex].scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+        });
+    }
 
 })
